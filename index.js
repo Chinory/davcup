2b28f99a167044e7b4f66d4daf9a4327f17bd18d
chinory <32269068+chinory@users.noreply.github.com>
2018-07-26 00:13:25 +0800

    user

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const path = require('path')
 
+const config = require('config-lite')(__dirname)
 const webdav = require('webdav-server').v2
 const helmet = require('helmet')
 const express = require('express')
 
-const server = new webdav.WebDAVServer()
 const app = express()
 
 // helmet
@@ -14,7 +14,25 @@ app.use(helmet())
 app.use(express.static(path.join(__dirname, 'public')))
 
 // webdav
+const userManager = new webdav.SimpleUserManager()
+const user = userManager.addUser(config.username, config.password, false)
+const privilegeManager = new webdav.SimplePathPrivilegeManager();
+privilegeManager.setRights(user, '/', [ 'all' ]);
+const server = new webdav.WebDAVServer({
+  privilegeManager: privilegeManager
+})
 app.use(webdav.extensions.express(path.join(__dirname, 'dav'), server))
 
+// log
+server.afterRequest((arg, next) => {
+  // Display the method, the URI, the returned status code and the returned message
+  console.log('>>', arg.request.method, arg.uri, '>', arg.response.statusCode, arg.response.statusMessage);
+  // If available, display the body of the response
+  console.log(arg.responseBody);
+  next();
+})
+
 // start
-app.listen(80)
+app.listen(config.port, () => {
+  console.log(`${pkg.name} listening on port ${config.port}`)
+})
